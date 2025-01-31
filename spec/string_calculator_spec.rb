ff0d1b14eeef7af53b0be4ead7dedef49d0fceb8
dhruvika29kevat <dhruvikakevat@gmail.com>
2025-01-31 20:20:08 +0530

    handeling delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("1")).to eq(1)
     end
+
+    it "returns the sum of the numbers seperated by a comma" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1,2")).to eq(3)
+      expect(calculator.add("10,20,30")).to eq(60)
+    end
+
+    it "handles the delimiters" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,15 @@
 class StringCalculator
   def add(numbers)
-      return 0 if numbers.empty?
-      numbers.to_i
+    return 0 if numbers.empty?
+
+    delimiter = /,|\n/
+
+    if numbers.start_with?("//")
+      delimiter = Regexp.escape(numbers[2])
+      numbers = numbers[4..]
+    end
+
+    num_list = numbers.split(/#{delimiter}/).map(&:to_i)
+    num_list.reject { |n| n > 1000 }.sum
   end
 end
